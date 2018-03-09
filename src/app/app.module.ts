3d28d64b008a1ca13b255412627ed10c1306594f
Leocaliban <leocaliban@gmail.com>
2018-03-09 12:29:23 -0300

    Cria tabela de dados na pesquisa de lançamentos]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import {InputTextModule} from 'primeng/inputtext';
 import {ButtonModule} from 'primeng/button';
+import {DataTableModule} from 'primeng/datatable';
 
 import { AppComponent } from './app.component';
 
@@ -14,8 +15,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     InputTextModule,
-    ButtonModule
-
+    ButtonModule,
+    DataTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
