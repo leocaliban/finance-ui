091c494e790a953702024f47b711481be579b7a0
Leocaliban <leocaliban@gmail.com>
2018-03-12 00:31:17 -0300

    Customiza colunas com ng-template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,9 @@ import { Component } from '@angular/core';
 export class AppComponent {
   lancamentos = [
     {tipo: 'DESPESA', descricao: 'Compra de pão', dataVencimento: '30/06/2017', dataPagamento: null, valor: 4.55
-      , pessoa: 'Padaria Pão Doido'}
+      , pessoa: 'Padaria Pão Doido'},
+    {tipo: 'RECEITA', descricao: 'Aluguel', dataVencimento: '11/03/2018', dataPagamento: '11/03/2018', valor: 220.00
+      , pessoa: 'Jack Bauer'}
   ];
 
 }
